59f84aba3a2e3f30087a382d3db861ed60c18e9b
rdavidson634 <rdavidson634@gmail.com>
2020-08-28 15:46:36 -0600

    setting up

---
+++ config/passport.js
@@ -0,0 +1,14 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const User = require('../models/user');
+
+// new code below
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    // a user has logged in with OAuth...
+  }
+));
--- server.js
+++ server.js
@@ -3,24 +3,49 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// session middleware
+var session = require('express-session');
+var passport = require('passport');
+var methodOverride = require('method-override');
 
+// env vars
+require('dotenv').config();
+
+// creating express app
+var app = express();
+
+// connect to the MongoDB with mongoose
+require('./config/database');
+// configure Passport
+require('./config/passport');
+
+// require routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(methodOverride('_method'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// mount the session middleware
+app.use(session({
+  secret: 'golfcation',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
