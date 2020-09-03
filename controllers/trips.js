7771c721771f9ce71904abac8857681401afd23d
rdavidson634 <rdavidson634@gmail.com>
2020-09-03 16:04:00 -0600

    edit btn

--- controllers/trips.js
+++ controllers/trips.js
@@ -28,7 +28,7 @@ function create (req, res) {
   trip.save(function(err) {
     if (err) return res.render('trips/new', { title: 'New Trip' });
     
-    res.redirect('/trips');
+    res.redirect('/trips/');
   })
 }
 
@@ -47,7 +47,7 @@ function deleteTrip (req, res) {
 function edit (req, res) {
   Trip.findById(req.params.id, function(err, trip) {
     // console.log(trip)
-    if (!trip.userId.equals(req.user._id)) return res.redirect('/trips/show');
+    if (!trip.userId.equals(req.user._id)) return res.redirect('/trips');
     res.render('trips/edit', {trip})
   })
 }
